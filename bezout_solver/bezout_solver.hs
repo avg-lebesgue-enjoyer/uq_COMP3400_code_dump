9ad782cb30c0330502761c44d4751812515c36a4
avg-lebesgue-enjoyer <gabriel.field@uqconnect.edu.au>
2024-02-20 18:04:13 +1000

    notNice

--- bezout_solver/bezout_solver.hs
+++ bezout_solver/bezout_solver.hs
@@ -18,10 +18,74 @@ main = putStrLn "placeholder"
 
 
 {- SECTION: IO interaction -}
+outCursor :: String
+outCursor = "out> "
+inCursor :: String
+inCursor = "in>  "
+
+speak :: String -> IO ()
+speak = putStrLn . (outCursor ++)
+
+request :: String -> IO String
+request prompt = do
+    speak prompt
+    putStr inCursor
+    getLine
+
+-- Helper: Receive a, b, c
+getEquation :: IO (Int, Int, Int)
+getEquation = do
+    speak "We will solve the equation   ax + by = c   for integers x and y, where..."
+    a' <- request "a = ..."
+    b' <- request "b = ..."
+    c' <- request "c = ..."
+    let a = read a' :: Int
+        b = read b' :: Int
+        c = read c' :: Int
+    speak $ "Your equation `is " ++ show a ++ "x + " ++ show b ++ "y = " ++ show c
+    return (a, b, c)
+
+-- Nicely formatted
+nice :: IO ()
+nice = do
+    -- Initialisation
+    (a, b, c) <- getEquation
+    putStrLn "placeholda"
+
+-- Not so nicely formatted working
+notNice :: IO ()
+notNice = do
+    -- Initialisation
+    (a, b, c) <- getEquation
+    -- Euclidean algorithm trace
+    let trace = euclidTrace a b
+    speak $ "The Euclidean algorithm progresses like follows: \n" ++ show trace
+    let theGcd =        gcd a b
+        gcdSol =        gcdSolution trace
+        problemSol =    solution gcdSol c
+        hasSolution =   problemSol /= MaybeSolution Nothing
+        solutionNot =   if hasSolution then "" else "not "
+    speak $ "The gcd of a and b is " ++ show theGcd
+    speak $ "The equation does " ++ solutionNot ++ "have a solution!"
+    if not hasSolution then do
+        return ()
+    else do
+        let baseEquation =  getBaseEvolvingEquation trace
+            extraInfo =     getInformationEquations trace
+            working =       workThrough trace
+            gcdSol =        gcdSolution trace
+            sol =           solution gcdSol c
+        -- Extended Euclidean algorithm
+        speak $ "The base equation is:\n" ++ show baseEquation
+        speak $ "The extra information we can use is as follows: \n" ++ show extraInfo
+        speak $ "Our working is: \n" ++ show working
+        speak $ "For the equation " ++ show a ++ "x + " ++ show b ++ "y = " ++ show theGcd ++ " we have:\n" ++ show gcdSol
+        speak $ "For the equation you actually asked about, we have:\n" ++ show sol
+        
 
 
 
-{- SECTION: Bezout solver -}
+{- SECTION: Euclidean algorithm trace -}
 type Big = Int
 type CoefficientQuotient = Int
 type Small = Int
@@ -61,3 +125,90 @@ euclidTrace' big small history =
 
 humanReadableEuclidTrace :: Int -> Int -> [EuclidStep]
 humanReadableEuclidTrace big = reverse . euclidTrace big
+
+
+
+{- SECTION: Extended Euclidean algo -}
+-- EvolvingEquation (g, p, y, a, z) represents the equation
+-- "g = p * y - a * z"
+newtype EvolvingEquation = EvolvingEquation (Int, Int, Int, Int, Int)
+instance Show EvolvingEquation where
+    show :: EvolvingEquation -> String
+    show (EvolvingEquation (g, p, y, a, z)) = "\t" ++ show g ++ "\t= " ++ show p ++ "\t* " ++ show y ++ "\t-\t" ++ show a ++ "\t* " ++ show z ++ "\n"
+
+-- InformationEquation (z, x, b, y) represents the equation
+-- "z = x - b * y"
+newtype InformationEquation = InformationEquation (Int, Int, Int, Int)
+instance Show InformationEquation where
+    show :: InformationEquation -> String
+    show (InformationEquation (z, x, b, y)) = "\t" ++ show z ++ "\t= " ++ show x ++ "\t\t-\t" ++ show b ++ "\t* " ++ show y ++ "\n"
+
+-- FinalEquation (g, p, y, b, z) represents the equation
+-- "g = p * y + b * z"
+-- A conversion b = -a converts from EvolvingEquation to FinalEquation
+newtype FinalEquation = FinalEquation (Int, Int, Int, Int, Int)
+instance Show FinalEquation where
+    show :: FinalEquation -> String
+    show (FinalEquation (g, p, y, b, z)) =
+        "\t" ++ show g ++ "\t= " ++ show p ++ "\t* " ++ show y ++ "\t+\t" ++ show b ++ "\t* " ++ show z ++ "\n" ++
+        "cf.\t" ++ show g ++ "\t= " ++ "x" ++ "\t* " ++ show y ++ "\t+\t" ++ "y"    ++ "\t* " ++ show z ++ "\n" ++
+        "=>\t" ++ "(x, y) \t= (" ++ show p ++ ", " ++ show b ++ ")"
+
+evolvingEquationToFinalEquation :: EvolvingEquation -> FinalEquation
+evolvingEquationToFinalEquation (EvolvingEquation (g, p, y, a, z)) =
+    FinalEquation (g, p, y, -a, z)
+
+-- Evolve an EvolvingEquation
+-- >> evolve ee ie << returns an EvolvingEquation which uses ie to introduce a new variable and kill one from ee.
+-- Assumes that ee and ie are compatible
+evolve :: EvolvingEquation -> InformationEquation -> EvolvingEquation
+evolve (EvolvingEquation (g, p, y, a, z)) (InformationEquation (z', x, b, y'))
+    | z /= z' || y /= y' =  error "this fucked up"
+    | otherwise =           EvolvingEquation (g, -a, x, -p - a*b, y)
+
+-- Grab base EvolvingEquation from a Euclid trace
+-- yeah, this is just the second element
+getBaseEvolvingEquation :: [EuclidStep] -> EvolvingEquation
+getBaseEvolvingEquation [] =    error "0 is too few steps!"
+getBaseEvolvingEquation [_] =   error "1 is too few steps!"
+getBaseEvolvingEquation (_:(EuclidStep (big, coefficient, small, remainder)):_) =
+    EvolvingEquation (remainder, 1, big, coefficient, small)
+
+euclidStepToInformationEquation :: EuclidStep -> InformationEquation
+euclidStepToInformationEquation (EuclidStep (big, coefficient, small, remainder)) =
+    InformationEquation (remainder, big, coefficient, small)
+
+getInformationEquations :: [EuclidStep] -> [InformationEquation]
+getInformationEquations = map euclidStepToInformationEquation . drop 2
+
+workThrough :: [EuclidStep] -> [EvolvingEquation]
+workThrough history =
+    scanl evolve (getBaseEvolvingEquation history) (getInformationEquations history)
+
+gcdSolution :: [EuclidStep] -> FinalEquation
+gcdSolution =
+    evolvingEquationToFinalEquation . last . workThrough
+
+
+
+{- SECTION: Solve generalised Bezout equation -}
+type Cee = Int
+newtype MaybeSolution = MaybeSolution (Maybe FinalEquation)
+instance Show MaybeSolution where
+    show :: MaybeSolution -> String
+    show (MaybeSolution Nothing) = "No solution!"
+    show (MaybeSolution (Just finalEquation)) = show finalEquation
+instance Eq MaybeSolution where
+    (==) :: MaybeSolution -> MaybeSolution -> Bool
+    (==) (MaybeSolution Nothing) (MaybeSolution Nothing) = True
+    (==) (MaybeSolution Nothing) _ = False
+    (==) _ (MaybeSolution Nothing) = False
+    (==) (MaybeSolution (Just s1)) (MaybeSolution (Just s2)) = show s1 == show s2
+
+-- >> solution finalEquation c << returns:
+--      FinalEquation c = whatever blah blah    if has solution
+--      Nothing                                 if doesn't
+solution :: FinalEquation -> Cee -> MaybeSolution
+solution (FinalEquation (g, p, y, b, z)) c
+    | c `mod` gcd y z /= 0 =    MaybeSolution Nothing
+    | otherwise =               MaybeSolution . Just $ FinalEquation (c, p * c `div` g, y, b * c `div` g, z)
