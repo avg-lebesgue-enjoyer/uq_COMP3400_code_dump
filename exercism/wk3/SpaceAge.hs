48bd70ee35d0beaf9e0a3b9bb8f9682f3aea2173
avg-lebesgue-enjoyer <gabriel.field@uqconnect.edu.au>
2024-02-28 09:02:34 +1000

    SpaceAge.hs

--- exercism/wk3/SpaceAge.hs
+++ exercism/wk3/SpaceAge.hs
@@ -10,4 +10,28 @@ data Planet = Mercury
             | Neptune
 
 ageOn :: Planet -> Float -> Float
-ageOn planet seconds = error "You need to implement this function."
+ageOn planet seconds = seconds / (orbitalPeriods !! planetIndex planet)
+
+-- index in orbitalPeriods
+planetIndex :: Planet -> Int
+planetIndex Mercury =   0
+planetIndex Venus =     1
+planetIndex Earth =     2
+planetIndex Mars =      3
+planetIndex Jupiter =   4
+planetIndex Saturn =    5
+planetIndex Uranus =    6
+planetIndex Neptune =   7
+
+-- orbital periods, in seconds.
+orbitalPeriods :: [Float]
+orbitalPeriods = map (* (365.25 * 24 * 60 * 60))
+    [   0.2408467,
+        0.61519726,
+        1.0,
+        1.8808158,
+        11.862615,
+        29.447498,
+        84.016846,
+        164.79132
+    ]
